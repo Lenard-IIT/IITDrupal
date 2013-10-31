52656373a8f9a1a84c745084d632f3b334346d33
Brian Bailey <bbailey4@iit.edu>
2013-10-31 12:31:52 -0500

    iit_stuart theme - modified faculty-alpha-list.js to wrap with a div

--- docroot/sites/stuart/themes/iit_stuart/scripts/faculty-alpha-list.js
+++ docroot/sites/stuart/themes/iit_stuart/scripts/faculty-alpha-list.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
 		$(this).attr("id", $(this).text());
 	});
 	var $letter_selector_container = $("<div>", {id: "letter-selector-wrapper"});
-	var $letter_selector = $('<ul>', {id: "letter-selector"});
+	var $letter_selector = $('<div>', {id: "letter-selector"});
 	$('.view-faculty h3').each(function(){
 		var id = $(this).attr("id");
 		var $aLink = $('<a>', {title: id, href: ('#' + id)}).html(id);
