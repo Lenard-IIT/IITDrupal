1426c810fcb36bfd14d7e1495e4be9f234de75dc
Brian Bailey <bbailey4@iit.edu>
2013-12-02 12:24:04 -0600

    iit_science theme - added search box js and css for search box and search page

---
+++ docroot/sites/science/themes/iit_science/scripts/search-box.js
@@ -0,0 +1,8 @@
+(function($){
+
+	$(document).ready(function(){
+		$('#search-block-form .form-item-search-block-form input.form-text').attr("placeholder", "Search Science");
+		$('#search-form #edit-keys').attr("placeholder", "Search Science");
+	});
+
+})(jQuery);
