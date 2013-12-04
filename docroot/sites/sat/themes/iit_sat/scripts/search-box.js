3cc421237da88a5a318c0e02b110d3af07ec10d7
Brian Bailey <bbailey4@iit.edu>
2013-12-04 10:26:50 -0600

    iit_sat theme - added some basic css to get to a similar starting point to other sites

---
+++ docroot/sites/sat/themes/iit_sat/scripts/search-box.js
@@ -0,0 +1,8 @@
+(function($){
+
+	$(document).ready(function(){
+		$('#search-block-form .form-item-search-block-form input.form-text').attr("placeholder", "Search Applied Technology");
+		$('#search-form #edit-keys').attr("placeholder", "Search Applied Technology");
+	});
+
+})(jQuery);
