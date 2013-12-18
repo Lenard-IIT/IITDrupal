ba77c0f823e12c316d329978f36cc1b840832944
Brian Bailey <bbailey4@iit.edu>
2013-12-18 08:58:28 -0600

    iit_humansci theme - added js and css for sidebar search block and search page

---
+++ docroot/sites/humanscience/themes/iit_humanscience/scripts/search-box.js
@@ -0,0 +1,8 @@
+(function($){
+
+	$(document).ready(function(){
+		$('#search-block-form .form-item-search-block-form input.form-text').attr("placeholder", "Search Lewis College");
+		$('#search-form #edit-keys').attr("placeholder", "Search Lewis College");
+	});
+
+})(jQuery);
