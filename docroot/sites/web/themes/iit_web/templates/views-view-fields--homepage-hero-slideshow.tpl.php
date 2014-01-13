1bba96df49a2c4ac7b0c15b924fab34c3606a2cf
Brian Bailey <bbailey4@iit.edu>
2014-01-13 09:40:18 -0600

    iit_web theme - added homepage templates

---
+++ docroot/sites/web/themes/iit_web/templates/views-view-fields--homepage-hero-slideshow.tpl.php
@@ -0,0 +1,7 @@
+<div class="slideshow-item">
+	<div class="slide-caption" style="background-color:<?php print $fields['field_slide_color']->content; ?>;">
+		<h2 class="slide-title"><a href="<?php print $fields['field_link_to']->content; ?>"><?php print $fields['title']->content; ?></a></h2>
+		<div class="slide-body"><a href="<?php print $fields['field_link_to']->content; ?>"><?php print $fields['body']->content; ?></a></div>
+	</div>
+	<div class="slide-image"><a href="<?php print $fields['field_link_to']->content; ?>"><?php print $fields['field_hero_image']->content; ?></a></div>
+</div>
