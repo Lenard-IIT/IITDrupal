731b6b073ae45a173a9b9ea0dbda55ff12416472
Brian T Bailey <briantbailey@gmail.com>
2014-01-15 14:30:59 -0600

    web site modules - installed iit_alert_message 7.x-0.9

---
+++ docroot/sites/web/modules/custom/iit_alert_message/iit_alert_message.js
@@ -0,0 +1,24 @@
+(function($) {
+	Drupal.behaviors.iit_alert_message = {
+		attach:function(context) {
+			if ($('#iit-alert-message', context).length) {
+				$.ajax({
+					url: '/api/v1/public/alerts',
+					success: function(data) {
+						if (data.nid !== '0') {
+							// Alert Message is present
+							var content = '<div class="inner-container"><div id="iit-alert-message-content">';
+							content += '<h1 class="message-title">' + data.title + '</h1>';
+							content += '<h3 class="message-updated">' + 'Updated ' + data.updatedDate + '</h3>';
+							content += '<div class="message-body">' + data.body_value + '</div>';
+							content += '</div></div>';
+
+							$('#iit-alert-message').addClass(data.alert_level.toLowerCase());
+							$('#iit-alert-message').html(content);
+						}
+					}
+				});
+			}
+		}
+	}
+})(jQuery);
